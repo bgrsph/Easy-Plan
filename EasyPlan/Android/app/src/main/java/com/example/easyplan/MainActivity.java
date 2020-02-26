aa74c6577c1f0b0ba6afb9cbad06bf3a4c02c296
Mel <57153939+MelAsAdaLovelace@users.noreply.github.com>
2020-02-27 00:27:15 +0300

    Rename XML

--- EasyPlan/Android/app/src/main/java/com/example/easyplan/MainActivity.java
+++ EasyPlan/Android/app/src/main/java/com/example/easyplan/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         logEmail = findViewById(R.id.logEmail);
         logPass = findViewById(R.id.logPassword);
         loginBtn = findViewById(R.id.logButton);
