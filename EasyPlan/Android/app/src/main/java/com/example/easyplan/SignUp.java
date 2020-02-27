f3a51ca19345033204e8191bf60a887a188e0101
iyi3rt <mertinimeyili@gmail.com>
2020-02-27 22:37:14 +0300

    signup page bug fixed

--- EasyPlan/Android/app/src/main/java/com/example/easyplan/SignUp.java
+++ EasyPlan/Android/app/src/main/java/com/example/easyplan/SignUp.java
@@ -32,24 +32,18 @@ public class SignUp extends AppCompatActivity {
         signupButton = findViewById(R.id.signupButton);
         error = findViewById(R.id.error);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     public void signUp (View view) {
         //implement try-catch ?
+        String a = password.getText().toString();
+        String b = passwordAgain.getText().toString();
         if(!email.getText().toString().contains("@ku.edu.tr")){
             error.setText("Please enter a valid KU e-mail address.");
             error.setVisibility(View.VISIBLE);
-        } else if (password.getText().toString() != passwordAgain.getText().toString()){
-            error.setText("The passwords do not match.");
-            error.setVisibility(View.VISIBLE);
+        } else if (!a.equals(b)){
+                error.setText("The passwords do not match.");
+                error.setVisibility(View.VISIBLE);
         } else {
             error.setVisibility(View.INVISIBLE);
             Intent intent = new Intent(SignUp.this, MainActivity.class);
