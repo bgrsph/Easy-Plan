7c8a55ffc2bbc72c8db7ebe6585a788d252f6b87
iyi3rt <mertinimeyili@gmail.com>
2020-03-05 10:24:45 +0300

    click events added for the bottom navbar

--- EasyPlan/Android/app/src/main/java/com/example/easyplan/Mainpage.java
+++ EasyPlan/Android/app/src/main/java/com/example/easyplan/Mainpage.java
@@ -1,25 +1,33 @@
 package com.example.easyplan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Mainpage extends AppCompatActivity {
 
     Button logout;
-
+    BottomNavigationView bottomMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
+        bottomMenu = findViewById(R.id.NavigationBot);
+        bottomMenu.setOnNavigationItemSelectedListener(NavigationItemSelectedListener);
         //logout = findViewById(R.id.logoutBtn);
 
 
@@ -32,5 +40,35 @@ public class Mainpage extends AppCompatActivity {
                // finish();
            // }
         //});
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener NavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener()
+    {
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item)
+        {
+            switch (item.getItemId())
+            {
+                case R.id.search:
+                    //fragment = new HomeFragment();
+                    Toast.makeText(Mainpage.this, "This is where you search for courses.", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.plans:
+                    //fragment = new InboxFragment();
+                    Toast.makeText(Mainpage.this, "This is where you see your plans.", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.help:
+                    //Intent intent = new Intent(this, AddActivity.class);
+                    Toast.makeText(Mainpage.this, "If you, for whatever stupid reason, cannot use this app, come here.", Toast.LENGTH_SHORT).show();
+                    //startActivity(intent);
+                    return true;
+                case R.id.settings:
+                    Toast.makeText(Mainpage.this, "To change your settings.", Toast.LENGTH_SHORT).show();
+                    //fragment = new HistoryFragment();
+                    break;
+            }
+            return false;
+        }
+    };
 }
