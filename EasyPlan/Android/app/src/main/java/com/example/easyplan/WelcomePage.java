6875f85fe93e56f2f01b3017bac6ea37337ed7a9
Mel <57153939+MelAsAdaLovelace@users.noreply.github.com>
2020-03-11 08:25:47 +0300

    Video on welcome page

--- EasyPlan/Android/app/src/main/java/com/example/easyplan/WelcomePage.java
+++ EasyPlan/Android/app/src/main/java/com/example/easyplan/WelcomePage.java
@@ -4,14 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class WelcomePage extends AppCompatActivity {
     Button signUpButton;
     Button loginButton;
+    VideoView vv;
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        vv =  (VideoView) findViewById(R.id.videoView);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.koc_tanitim;
+        Uri uri = Uri.parse(videoPath);
+        vv.setVideoURI(uri);
+        vv.start();
+
+
+        vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(true);
+
+            }
+
+        });
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +45,22 @@ public class WelcomePage extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
         signUpButton = findViewById(R.id.welcomeSignUP);
         loginButton = findViewById(R.id.welcomeLogin);
+
+        vv =  (VideoView) findViewById(R.id.videoView);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.koc_tanitim;
+        Uri uri = Uri.parse(videoPath);
+        vv.setVideoURI(uri);
+        vv.start();
+
+
+        vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(true);
+
+            }
+
+        });
 
         SharedPreferences sp = getSharedPreferences("checkbox", MODE_PRIVATE);
         String remembered = sp.getString("remember", "");
