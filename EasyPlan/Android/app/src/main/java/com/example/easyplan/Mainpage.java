a683714dd21d326ccb44e4e273ea909bdf06979a
Mel <57153939+MelAsAdaLovelace@users.noreply.github.com>
2020-03-11 06:52:33 +0300

    Bottom Navigator Completed
    All pages will be fragments.

--- EasyPlan/Android/app/src/main/java/com/example/easyplan/Mainpage.java
+++ EasyPlan/Android/app/src/main/java/com/example/easyplan/Mainpage.java
@@ -2,23 +2,15 @@ package com.example.easyplan;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -41,17 +33,20 @@ public class Mainpage extends AppCompatActivity {
         setContentView(R.layout.activity_mainpage);
         bottomMenu = findViewById(R.id.NavigationBot);
         bottomMenu.setOnNavigationItemSelectedListener(NavigationItemSelectedListener);
+        ClassSearchFragment classSearchFragment = new ClassSearchFragment();
+        FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction1.replace(R.id.fragment, classSearchFragment, "ClassSearch");
+        fragmentTransaction1.commit();
     }
 
 
-    private BottomNavigationView.OnNavigationItemSelectedListener NavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener()
-    {
+    private BottomNavigationView.OnNavigationItemSelectedListener NavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener(){
         @Override
-        public boolean onNavigationItemSelected(MenuItem item)
-        {
+        public boolean onNavigationItemSelected(MenuItem item){
             switch (item.getItemId()){
                 case R.id.search:
-                    //fragment = new HomeFragment();
+          /*          //fragment = new HomeFragment();
                     //Toast.makeText(Mainpage.this, "This is where you search for courses.", Toast.LENGTH_SHORT).show();
                     mDatabase = FirebaseDatabase.getInstance();
 
@@ -68,28 +63,37 @@ public class Mainpage extends AppCompatActivity {
                         public void onCancelled(@NonNull DatabaseError databaseError) {
 
                         }
-                    });
-                    bottomMenu.getMenu().getItem(0).setChecked(true);
-                    break;
+                    });*/
+                    ClassSearchFragment classSearchFragment = new ClassSearchFragment();
+                    FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction1.replace(R.id.fragment, classSearchFragment, "ClassSearch");
+                    fragmentTransaction1.commit();
+                    return true;
                 case R.id.plans:
                     //fragment = new InboxFragment();
-                    Toast.makeText(Mainpage.this, "This is where you see your plans.", Toast.LENGTH_SHORT).show();
-                    bottomMenu.getMenu().getItem(1).setChecked(true);
-                    break;
+//                    Toast.makeText(Mainpage.this, "This is where you see your plans.", Toast.LENGTH_SHORT).show();
+                    PlansFragment plansFragment = new PlansFragment();
+                    FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction2.replace(R.id.fragment, plansFragment, "Plans");
+                    fragmentTransaction2.commit();
+                    return true;
                 case R.id.help:
                     //Intent intent = new Intent(this, AddActivity.class);
-                    Toast.makeText(Mainpage.this, "If you, for whatever stupid reason, cannot use this app, come here.", Toast.LENGTH_SHORT).show();
-                    bottomMenu.getMenu().getItem(2).setChecked(true);
-                    //startActivity(intent);
+//                    Toast.makeText(Mainpage.this, "If you, for whatever stupid reason, cannot use this app, come here.", Toast.LENGTH_SHORT).show();
+                    HelpFragment helpFragment = new HelpFragment();
+                    FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction3.replace(R.id.fragment, helpFragment, "Plans");
+                    fragmentTransaction3.commit();
                     return true;
                 case R.id.profile:
-                    Toast.makeText(Mainpage.this, "To change your settings.", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(Mainpage.this, Profile.class));
-                    bottomMenu.getMenu().getItem(3).setChecked(true);
-                    //fragment = new HistoryFragment();
-                    break;
+//                    Toast.makeText(Mainpage.this, "To change your settings.", Toast.LENGTH_SHORT).show();
+                    ProfileFragment profileFragment = new ProfileFragment();
+                    FragmentTransaction fragmentTransaction4 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction4.replace(R.id.fragment, profileFragment, "Plans");
+                    fragmentTransaction4.commit();
+                    return true;
             }
-            return false;
+            return true;
         }
     };
 
