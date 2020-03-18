ea331d91e045aa2337fbcea2eb9e4530add72a08
Sitare <sitare.arslanturk@gmail.com>
2020-03-19 02:39:37 +0300

    Search Bar Function Fixed

--- EasyPlan/iOS/EasyPlaniOS/EasyPlaniOS/ViewControllers/HomeViewController.swift
+++ EasyPlan/iOS/EasyPlaniOS/EasyPlaniOS/ViewControllers/HomeViewController.swift
@@ -10,62 +10,74 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
-   let timePicker = UIDatePicker()
-    let courses = ["comp491", "comp303", "comp302", "comp301", "comp304", "comp305", "acct201", "acct202"]
+    let timePicker = UIDatePicker()
+    let courses = ["comp491", "comp303", "comp302", "comp301", "comp304", "comp305", "acct201", "acct202", "ethr113","hums108"]
+    var searchCourse = [String]()
+    var filtered:[String] = []
+    var searching = false
     @IBOutlet weak var searchBar: UISearchBar!
-   
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchCourseButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        setUpNavBar()
+        setUpSearchBar()
         self.navigationController?.navigationBar.prefersLargeTitles = true
-       
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
+     
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Hide the navigation bar on the this view controller
-        self.tabBarController?.navigationController?.setNavigationBarHidden(true, animated: animated)
+self.tabBarController?.navigationController?.setNavigationBarHidden(true, animated: animated)
         
     }
     
-    func setUpNavBar(){
-        navigationItem.hidesSearchBarWhenScrolling = false
-//        öteki VC için
-//        navigationItem.largeTitleDisplayMode = .never
-//        let searchController = UISearchController(searchResultsController: nil)
-//        navigationItem.searchController = searchController
-//
-//        searchController.automaticallyShowsCancelButton = true
-       
-        
+    func setUpSearchBar(){
+        searchBar.placeholder = "Search"
+//        navigationItem.hidesSearchBarWhenScrolling = false
+        //        öteki VC için
+        //        navigationItem.largeTitleDisplayMode = .never
     }
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
 
 extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searching {
+            return searchCourse.count
+        }
         return courses.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
-        cell?.textLabel?.text = courses[indexPath.row]
-        return cell!
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        if searching {
+            cell?.textLabel?.text = searchCourse[indexPath.row]
+        } else {
+            cell?.textLabel?.text = courses[indexPath.row]
+        }
+        return cell!
+        
+    }
+}
+
+extension HomeViewController: UISearchBarDelegate{
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        let text = searchText.lowercased()
+        searchCourse = courses.filter({$0.prefix(text.count) == text})
+        searching = true
+        tableView.reloadData()
+    }
     
 }
