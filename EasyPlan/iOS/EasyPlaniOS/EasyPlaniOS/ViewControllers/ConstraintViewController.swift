787c8c287ba2581bd265dfd9adfe91cc8beb8595
Sitare <sitare.arslanturk@gmail.com>
2020-03-21 16:36:12 +0300

    View changed for course search page

---
+++ EasyPlan/iOS/EasyPlaniOS/EasyPlaniOS/ViewControllers/ConstraintViewController.swift
@@ -0,0 +1,126 @@
+//
+//  ConstraintViewController.swift
+//  EasyPlaniOS
+//
+//  Created by Sitare Arslantürk on 21.03.2020.
+//  Copyright © 2020 Easy Plan. All rights reserved.
+//
+
+import UIKit
+
+class ConstraintViewController: UIViewController {
+    @IBOutlet weak var planLabel: UITextField!
+    @IBOutlet weak var startLabel: UITextField!
+    @IBOutlet weak var finishLabel: UITextField!
+    
+    let start_picker = UIPickerView()
+    let start_time = ["8:30", "10:00", "11:30", "13:00", "14:30", "16:00", "17:30"]
+    
+    let finish_picker = UIPickerView()
+    let finish_time = ["9:45", "11:15", "12:45", "14:15", "15:45", "17.15", "18.45"]
+    
+    let plan_picker = UIPickerView()
+    let plan_no = ["Plan 1", "Plan 2", "Plan 3", "Plan 4", "Plan 5", "Plan 6", "Plan 7", "Plan 8", "Plan 9", "Plan 10"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        createPicker()
+        
+        start_picker.delegate = self
+        start_picker.dataSource = self
+        
+        finish_picker.delegate = self
+        finish_picker.dataSource = self
+        
+        plan_picker.delegate = self
+        plan_picker.dataSource = self
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func createPicker(){
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        //        bar done button
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelPressed))
+        toolbar.setItems([cancelButton,spaceButton,doneButton], animated: true)
+        //    assign toolbar
+        startLabel.inputAccessoryView = toolbar
+        startLabel.inputView = start_picker
+        
+        finishLabel.inputAccessoryView = toolbar
+        finishLabel.inputView = finish_picker
+        
+        planLabel.inputAccessoryView = toolbar
+        planLabel.inputView = plan_picker
+    }
+    
+    @objc func cancelPressed(){
+        self.view.endEditing(true)
+        startLabel.text = ""
+    }
+    
+    //        @objc func dateChanged(timePicker: UIDatePicker){
+    //            let dateFormatter = DateFormatter()
+    //            dateFormatter.timeStyle = .short
+    //            timeslotTextField.text = dateFormatter.string(from: timePicker.date)
+    //
+    //        }
+    
+    @objc func donePressed(){
+        self.view.endEditing(true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+extension ConstraintViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == finish_picker {
+            return finish_time.count
+        } else if pickerView == plan_picker {
+            return plan_no.count
+        } else {
+            return start_time.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == finish_picker {
+            return finish_time[row]
+        } else if pickerView == plan_picker {
+            return plan_no[row]
+        } else {
+            return start_time[row]
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == finish_picker {
+            finishLabel.text = finish_time[row]
+        } else if pickerView == plan_picker {
+            planLabel.text = plan_no[row]
+            //        self.view.endEditing(true)
+        } else {
+            startLabel.text = start_time[row]
+        }
+    }
+}
--- EasyPlan/iOS/EasyPlaniOS/EasyPlaniOS/ViewControllers/HomeViewController.swift
+++ EasyPlan/iOS/EasyPlaniOS/EasyPlaniOS/ViewControllers/HomeViewController.swift
@@ -10,25 +10,14 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
-    let start_picker = UIPickerView()
-    let start_time = ["8:30", "10:00", "11:30", "13:00", "14:30", "16:00", "17:30"]
+    var courses = [ Course(selected: false, name: "Comp491"), Course(selected: false, name: "Comp130"), Course(selected: false, name: "Comp131"),Course(selected: false, name: "Comp132"), Course(selected: false, name: "Comp200"),Course(selected: false, name: "Comp202"), Course(selected: false, name: "Comp301"), Course(selected: false, name: "Comp302"),Course(selected: false, name: "Comp303"), Course(selected: false, name: "Comp304"),Course(selected: false, name: "Comp305"), Course(selected: false, name: "Comp306"),Course(selected: false, name: "Ethr113"),Course(selected: false, name: "Ethr102"), Course(selected: false, name: "Ethr105"), Course(selected: false, name: "Acct201"), Course(selected: false, name: "Acct202")]
     
-    let finish_picker = UIPickerView()
-    let finish_time = ["9:45", "11:15", "12:45", "14:15", "15:45", "17.15", "18.45"]
-    
-    let plan_picker = UIPickerView()
-    let plan_no = ["Plan 1", "Plan 2", "Plan 3", "Plan 4", "Plan 5", "Plan 6", "Plan 7", "Plan 8", "Plan 9", "Plan 10"]
-    
-    var courses = [ Course(selected: false, name: "Comp491"), Course(selected: false, name: "Comp319"), Course(selected: false, name: "Comp130"),Course(selected: false, name: "Comp305"), Course(selected: false, name: "Comp301"),Course(selected: false, name: "Comp303"), Course(selected: false, name: "Comp302"),Course(selected: false, name: "Ethr113"),Course(selected: false, name: "Ethr102"), Course(selected: false, name: "Ethr105"), Course(selected: false, name: "Acct201"), Course(selected: false, name: "Acct202")]
-    
+    var selectedCourses:Int = 0
     var searchCourse = [Course]()
     var filtered:[String] = []
     var searching = false
     @IBOutlet weak var searchBar: UISearchBar!
     
-    @IBOutlet weak var planLabel: UITextField!
-    @IBOutlet weak var finishLabel: UITextField!
-    @IBOutlet weak var startLabel: UITextField!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchCourseButton: UIButton!
     
@@ -41,18 +30,11 @@ class HomeViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         
+        setUpHeader()
+
         searchBar.delegate = self
         
-        start_picker.delegate = self
-        start_picker.dataSource = self
-        
-        finish_picker.delegate = self
-        finish_picker.dataSource = self
-        
-        plan_picker.delegate = self
-        plan_picker.dataSource = self
-        
-        createPicker()
+
         
     }
     
@@ -64,6 +46,19 @@ class HomeViewController: UIViewController {
         
     }
     
+    func setUpHeader(){
+        let header = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 45))
+        header.layer.borderColor = UIColor(red:0.72, green:0.00, blue:0.00, alpha:1.00).cgColor
+        header.layer.borderWidth = 5
+        let selectionLabel = UILabel(frame: header.bounds)
+        selectionLabel.text = "0 Course Selected"
+        selectionLabel.textColor = UIColor(red:0.72, green:0.00, blue:0.00, alpha:1.00)
+        selectionLabel.textAlignment = .center
+        header.addSubview(selectionLabel)
+        tableView.tableHeaderView = header
+    }
+    
+    
     func setUpSearchBar(){
         searchBar.placeholder = "Search"
         //        navigationItem.hidesSearchBarWhenScrolling = false
@@ -71,40 +66,9 @@ class HomeViewController: UIViewController {
         //        navigationItem.largeTitleDisplayMode = .never
     }
     
-    func createPicker(){
-        let toolbar = UIToolbar()
-        toolbar.sizeToFit()
-        
-        //        bar done button
-        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
-        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelPressed))
-        toolbar.setItems([cancelButton,spaceButton,doneButton], animated: true)
-        //    assign toolbar
-        startLabel.inputAccessoryView = toolbar
-        startLabel.inputView = start_picker
-        
-        finishLabel.inputAccessoryView = toolbar
-        finishLabel.inputView = finish_picker
-        
-        planLabel.inputAccessoryView = toolbar
-        planLabel.inputView = plan_picker
-    }
     
-    @objc func cancelPressed(){
-        self.view.endEditing(true)
-        startLabel.text = ""
-    }
-    
-    //        @objc func dateChanged(timePicker: UIDatePicker){
-    //            let dateFormatter = DateFormatter()
-    //            dateFormatter.timeStyle = .short
-    //            timeslotTextField.text = dateFormatter.string(from: timePicker.date)
-    //
-    //        }
-    
-    @objc func donePressed(){
-        self.view.endEditing(true)
+    @IBAction func nextTapped(_ sender: Any) {
+        self.performSegue(withIdentifier: "constraint", sender: self)
     }
 }
 
@@ -123,7 +87,7 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CourseSearchTableViewCell
-        
+
         if searching {
             cell.courseNameLabel.text = searchCourse[indexPath.row].name
             if searchCourse[indexPath.row].selected {
@@ -145,8 +109,10 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
         if tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCell.AccessoryType.checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.none
+            selectedCourses -= 1
             if searching {
                 searchCourse[indexPath.row].selected = false
             } else {
@@ -154,12 +120,15 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
             }
         } else {
             tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.checkmark
+            selectedCourses += 1
             if searching {
                 searchCourse[indexPath.row].selected = true
             } else {
                 courses[indexPath.row].selected = true
             }
         }
+        
+        print(selectedCourses)
     }
     
     //    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
@@ -187,42 +156,5 @@ extension HomeViewController: UISearchBarDelegate{
 extension HomeViewController: TableViewNew {
     func onClickCellInfo(index: Int) {
         print("Info Clicked")
-    }
-}
-
-extension HomeViewController: UIPickerViewDataSource, UIPickerViewDelegate {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        if pickerView == finish_picker {
-            return finish_time.count
-        } else if pickerView == plan_picker {
-            return plan_no.count
-        } else {
-            return start_time.count
-        }
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if pickerView == finish_picker {
-            return finish_time[row]
-        } else if pickerView == plan_picker {
-            return plan_no[row]
-        } else {
-            return start_time[row]
-        }
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if pickerView == finish_picker {
-            finishLabel.text = finish_time[row]
-        } else if pickerView == plan_picker {
-            planLabel.text = plan_no[row]
-            //        self.view.endEditing(true)
-        } else {
-            startLabel.text = start_time[row]
-        }
     }
 }
